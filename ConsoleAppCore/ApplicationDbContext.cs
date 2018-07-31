e0005f02eebedfadab2d6ab4044cb9827876656d
Antonio David Restrepo Carvajal <desarrollador@unicoc.edu.co>
2018-07-31 16:21:11 -0500

    UpDate Pc1 Unicoc

---
+++ ConsoleAppCore/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConsoleAppCore
+{
+    class ApplicationDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-K88TBLE;Initial Catalog=PruebaBd;Integrated Security=True")
+                .EnableSensitiveDataLogging(true)
+                .UseLoggerFactory(new LoggerFactory().AddConsole((category,level)=> level == LogLevel.Information && category == DbLoggerCategory.Database.Command.Name, true));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            //modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Entity<EstudianteCurso>().HasKey(x => new { x.CursoID, x.EstudianteId});
+
+            modelBuilder.Entity<Estudiante>().HasOne(d => d.Detalles).WithOne(e => e.Estudiante)
+                .HasForeignKey<EstudianteDetalle>(x => x.Id);
+            modelBuilder.Entity<EstudianteDetalle>().ToTable("Estudiante");
+
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Direccion> Direcciones { get; set; }
+        public DbSet<Institucion> Instituciones { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<EstudianteCurso> EstudiantesCursos { get; set; }
+    }
+}
+
+
--- ConsoleAppCore/Program.cs
+++ ConsoleAppCore/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace ConsoleAppCore
 {
@@ -7,6 +11,110 @@ namespace ConsoleAppCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var _nombre = "Martin Olmedo Restrepo Carvajal";
+            var _id = 1;
+            var _edad = 20;
+
+            using (var context = new ApplicationDbContext())
+            {
+                InsertEstudiantes(_nombre, _edad);
+            }
+            Console.WriteLine("Listo!");
         }
+
+
+        static void InsertEstudiantes(string _nombre, int _edad)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var estudiante = new Estudiante()
+                {
+                    Nombre = _nombre,
+                    Edad = _edad
+                };
+
+                var direccion = new Direccion()
+                {
+                    Calle = "Calle Caracas"
+                };
+
+                estudiante.Direccion = direccion;
+                BuscarEstudiantes("");
+                context.Add(estudiante);
+                context.SaveChanges();
+            }
+        }
+
+        static void BuscarEstudiantes(string _nombre)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                if (_nombre == null)
+                {
+                    var estudiante = context.Estudiantes.Where(n => n.Nombre == _nombre).ToList();
+                }
+                else
+                {
+                    var estudiente = context.Estudiantes.Include(d => d.Direccion).ToList();
+                }
+            }
+        }
+    }
+
+    [Table("Estudiante")]
+    class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public virtual Direccion Direccion { get; set; }
+        public int InstitucionId { get; set; }
+        public virtual Institucion Institucion { get; set; }
+        public virtual EstudianteCurso GetEstudiantesCursos { get; set; }
+        public virtual EstudianteDetalle Detalles { get; set; }
+    }
+
+
+    class EstudianteDetalle
+    {
+        public int Id { get; set; }
+        public bool Becado { get; set; }
+        public string Carrera { get; set; }
+        public int CategoriaDePago { get; set; }
+        public virtual Estudiante Estudiante { get; set; }
+    }
+
+    [Table("Direccion")]
+    class Direccion
+    {
+        public int Id { get; set; }
+        public string Calle { get; set; }
+        public int EstudianteId { get; set; }
+    }
+
+    [Table("Institucion")]
+    class Institucion
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Estudiante> Estudiantes { get; set; }
+    }
+
+    [Table("Curso")]
+    class Curso
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public virtual EstudianteCurso GetEstudiantesCursos { get; set; }
+}
+
+    [Table("EstudianteCurso")]
+    class EstudianteCurso
+    {
+        public int EstudianteId { get; set; }
+        public int CursoID { get; set; }
+        public bool Activo { get; set; }
+        public virtual Estudiante Estudiante { get; set; }
+        public virtual Curso Curso { get; set; }
     }
 }
